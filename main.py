e3b1dbd99e9c4d5625e5c54716778102e26c0ec6
DenverCoder2009 <76412568+DenverCoder2009@users.noreply.github.com>
2023-04-18 13:58:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,20 +18,23 @@ while game_is_on:
     snake_screen.screen.onkey(key="Left", fun=my_snake.move_left)
     snake_screen.screen.onkey(key="Right", fun=my_snake.move_right)
     my_snake.move_snake()
-    time.sleep(.1)
+    time.sleep(.05)
     snake_screen.screen.update()
 
+    # Identifies collision with a food piece
     if my_snake.snake_head.distance(food) < 15:
         scoreboard.increment_score()
         my_snake.create_body_part()
         food.refresh()
         print("nomomomomomomom")
 
+    # Identifies collision with the walls
     if my_snake.snake_head.xcor() > 280 or my_snake.snake_head.xcor() < -290 or my_snake.snake_head.ycor() > 280 or \
             my_snake.snake_head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
 
+    # identifies collision with the snakes own body parts.
     for item in my_snake.body_parts_list[1:]:
         if my_snake.snake_head.distance(item) < 10:
             game_is_on = False
